7a8f60ab755e243451c23380e8389a5f371fbcb4
Narazaka <info@narazaka.net>
2019-02-20 11:39:22 +0900

    fix test

--- test/callback.ts
+++ test/callback.ts
@@ -15,28 +15,28 @@ const request2 = new ShioriJK.Message.Request({request_line: {version: "2.6", me
 describe("wrapRequestCallback", () => {
   describe("request acceptance", () => {
     context("SHIORI/3.x", () => {
-      it("causes OK", async () => assert.equal(
+      it("causes OK", async () => assert.strictEqual(
         (await wrapRequestCallback(() => 1)(request3)).status_line.code,
         200,
       ));
     });
 
     context("SHIORI/2.x", () => {
-      it("causes Bad Request", async () => assert.equal(
+      it("causes Bad Request", async () => assert.strictEqual(
         (await wrapRequestCallback(() => 1)(request2)).status_line.code,
         400,
       ));
     });
 
     context("invalid request", () => {
-      it("causes Bad Request", async () => assert.equal(
+      it("causes Bad Request", async () => assert.strictEqual(
         (await wrapRequestCallback(() => 1)("foo")).status_line.code,
         400,
       ));
     });
 
     context("SHIORI/3.x object", () => {
-      it("causes OK", async () => assert.equal(
+      it("causes OK", async () => assert.strictEqual(
         (await wrapRequestCallback(() => 1)(request3obj)).status_line.code,
         200,
       ));
@@ -72,40 +72,40 @@ describe("wrapRequestCallback", () => {
 
     function describeCallbacks(type: "sync" | "async", callbacks: typeof syncCallbacks | typeof asyncCallbacks) {
       describe(`${type} callback`, function() {
-        it("accepts string value", async () => assert.equal(
+        it("accepts string value", async () => assert.strictEqual(
           (await wrapRequestCallback(callbacks.string)(request3)).headers.get("Value"),
           "str",
         ));
-        it("accepts empty string value", async () => assert.equal(
+        it("accepts empty string value", async () => assert.strictEqual(
           (await wrapRequestCallback(callbacks.emptyString)(request3)).status_line.code,
           204,
         ));
-        it("accepts number value", async () => assert.equal(
+        it("accepts number value", async () => assert.strictEqual(
           (await wrapRequestCallback(callbacks.number)(request3)).headers.get("Value"),
           "42",
         ));
-        it("accepts zero number value", async () => assert.equal(
+        it("accepts zero number value", async () => assert.strictEqual(
           (await wrapRequestCallback(callbacks.zeroNumber)(request3)).headers.get("Value"),
           "0",
         ));
-        it("accepts null value", async () => assert.equal(
+        it("accepts null value", async () => assert.strictEqual(
           // tslint:disable-next-line no-any
           (await wrapRequestCallback(callbacks.null as any)(request3)).status_line.code,
           204,
         ));
-        it("accepts undefined value", async () => assert.equal(
+        it("accepts undefined value", async () => assert.strictEqual(
           (await wrapRequestCallback(callbacks.undefined)(request3)).status_line.code,
           204,
         ));
-        it("accepts void", async () => assert.equal(
+        it("accepts void", async () => assert.strictEqual(
           (await wrapRequestCallback(callbacks.void)(request3)).status_line.code,
           204,
         ));
-        it("accepts error", async () => assert.equal(
+        it("accepts error", async () => assert.strictEqual(
           (await wrapRequestCallback(callbacks.error)(request3)).status_line.code,
           500,
         ));
-        it("accepts response", async () => assert.deepEqual(
+        it("accepts response", async () => assert.deepStrictEqual(
           new ShioriJK.Message.Response({
             status_line: {version: "3.0", code: 200},
             headers: {
@@ -122,7 +122,7 @@ describe("wrapRequestCallback", () => {
 
   describe("defaultHeaders", function() {
     context("no default headers", function() {
-      it("is same", async () => assert.deepEqual(
+      it("is same", async () => assert.deepStrictEqual(
         {Value: "1"},
         (await wrapRequestCallback(() => 1)(request3)).headers.header),
       );
@@ -130,14 +130,14 @@ describe("wrapRequestCallback", () => {
     context("with default headers", function() {
       context("no same headers in return value", () => {
         const defaultHeaders = {To: "sakura"};
-        it("appended", async () => assert.deepEqual(
+        it("appended", async () => assert.deepStrictEqual(
           {Value: "1", To: "sakura"},
           (await wrapRequestCallback(() => 1, defaultHeaders)(request3)).headers.header),
         );
       });
       context("same headers exist in return value", () => {
         const defaultHeaders = {Value: "sakura"};
-        it("is not overwrited", async () => assert.deepEqual(
+        it("is not overwrited", async () => assert.deepStrictEqual(
           {Value: "1"},
           (await wrapRequestCallback(() => 1, defaultHeaders)(request3)).headers.header),
         );
@@ -146,12 +146,12 @@ describe("wrapRequestCallback", () => {
         it("changes response", async () => {
           const defaultHeaders = {Charset: "Shift_JIS"};
           const callback = wrapRequestCallback(() => 1, defaultHeaders);
-          assert.deepEqual(
+          assert.deepStrictEqual(
             {Value: "1", Charset: "Shift_JIS"},
             (await callback(request3)).headers.header,
           );
           defaultHeaders.Charset = "UTF-8";
-          assert.deepEqual(
+          assert.deepStrictEqual(
             {Value: "1", Charset: "UTF-8"},
             (await callback(request3)).headers.header,
           );
@@ -164,7 +164,7 @@ describe("wrapRequestCallback", () => {
 describe("wrapRequestStringCallback", () => {
   const callback = () => 1;
 
-  it("same response as wrapRequestCallback", async () => assert.equal(
+  it("same response as wrapRequestCallback", async () => assert.strictEqual(
     (await wrapRequestCallback(callback)(request3)).toString(),
     (await wrapRequestStringCallback(callback)(request3)),
   ));
